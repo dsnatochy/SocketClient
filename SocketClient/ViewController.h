1c9d57c5db42fbe566d4e6a64fbb04ce1721f3f5
dsnatochy <dennis@poynt.co>
2016-02-18 15:18:57 -0800

    Initial Commit

---
+++ SocketClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketClient
+//
+//  Created by Dzianis Natochy on 2/18/16.
+//  Copyright © 2016 Poynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocketClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocketClient
+//
+//  Created by Dzianis Natochy on 2/18/16.
+//  Copyright © 2016 Poynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
