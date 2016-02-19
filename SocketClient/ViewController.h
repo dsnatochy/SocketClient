d4db702f0496b47c9e942651207344c3fe1d4fde
dsnatochy <dennis@poynt.co>
2016-02-18 18:40:10 -0800

    simple ios client

--- SocketClient/ViewController.h
+++ SocketClient/ViewController.h
@@ -8,8 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<NSStreamDelegate>
+{
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    
+    NSInputStream   *inputStream;
+    NSOutputStream  *outputStream;
+    
+    NSMutableArray  *messages;
+}
 
+@property (weak, nonatomic) IBOutlet UITextField *ipAddressText;
+@property (weak, nonatomic) IBOutlet UITextField *portText;
+@property (weak, nonatomic) IBOutlet UITextField *dataToSendText;
+@property (weak, nonatomic) IBOutlet UILabel *connectedLabel;
+@property (weak, nonatomic) IBOutlet UITextField *dataReceived;
+
+@property (weak, nonatomic) IBOutlet UITextView *requestTextArea;
+
+@property (weak, nonatomic) IBOutlet UIButton *sendMsgBtn;
+
+@property (weak, nonatomic) IBOutlet UITextView *responseText;
+@property (weak, nonatomic) IBOutlet UIButton *connectBtn;
 
 @end
 
--- SocketClient/ViewController.m
+++ SocketClient/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  SocketClient
-//
-//  Created by Dzianis Natochy on 2/18/16.
-//  Copyright © 2016 Poynt. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,7 +8,149 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //_connectedLabel.text = @"Disconnected";
+}
+
+- (IBAction) sendMessage {
+    
+    NSString *response  = [NSString stringWithFormat:@"%@\n\n", _requestTextArea.text];
+    NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+    [outputStream write:[data bytes] maxLength:[data length]];
+    [_ipAddressText resignFirstResponder];
+    
+    
+}
+
+- (void) messageReceived:(NSString *)message {
+    
+    [messages addObject:message];
+    
+    _responseText.text = message;
+    NSLog(@"%@", message);
+    
+    UIAlertController *myAlertController = [UIAlertController alertControllerWithTitle:@"Poynt Response"
+                                                              message: message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* ok = [UIAlertAction
+                         actionWithTitle:@"OK"
+                         style:UIAlertActionStyleDefault
+                         handler:^(UIAlertAction * action)
+                         {
+                             //Do some thing here, eg dismiss the alertwindow
+                             [myAlertController dismissViewControllerAnimated:YES completion:nil];
+                             
+                         }];
+    [myAlertController addAction: ok];
+    [self presentViewController:myAlertController animated:YES completion:nil];
+    [_sendMsgBtn setEnabled:NO];
+    [_connectBtn setEnabled:YES];
+
+}
+
+- (void)stream:(NSStream *)theStream handleEvent:(NSStreamEvent)streamEvent {
+    
+    NSLog(@"stream event %lu", streamEvent);
+    
+    switch (streamEvent) {
+            
+        case NSStreamEventOpenCompleted:
+            NSLog(@"Stream opened");
+            _connectedLabel.text = @"Connected";
+            break;
+        case NSStreamEventHasBytesAvailable:
+            
+            if (theStream == inputStream)
+            {
+                uint8_t buffer[1024];
+                NSInteger len;
+                
+                while ([inputStream hasBytesAvailable])
+                {
+                    len = [inputStream read:buffer maxLength:sizeof(buffer)];
+                    if (len > 0)
+                    {
+                        NSString *output = [[NSString alloc] initWithBytes:buffer length:len encoding:NSASCIIStringEncoding];
+                        
+                        if (nil != output)
+                        {
+                            NSLog(@"server said: %@", output);
+                            [self messageReceived:output];
+                        }
+                    }
+                }
+            }
+            break;
+            
+        case NSStreamEventHasSpaceAvailable:
+            NSLog(@"Stream has space available now");
+            break;
+            
+        case NSStreamEventErrorOccurred:
+            NSLog(@"%@",[theStream streamError].localizedDescription);
+            break;
+            
+        case NSStreamEventEndEncountered:
+            
+            [theStream close];
+            [theStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+            _connectedLabel.text = @"Disconnected";
+            NSLog(@"close stream");
+            break;
+        default:
+            NSLog(@"Unknown event");
+    }
+    
+}
+
+- (IBAction)connectToServer:(id)sender {
+    
+    NSLog(@"Setting up connection to %@ : %i", _ipAddressText.text, [_portText.text intValue]);
+    CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, (__bridge CFStringRef) _ipAddressText.text, [_portText.text intValue], &readStream, &writeStream);
+    
+    messages = [[NSMutableArray alloc] init];
+    
+    [self open];
+}
+
+- (IBAction)disconnect:(id)sender {
+    
+    [self close];
+}
+
+- (void)open {
+    
+    NSLog(@"Opening streams.");
+    
+    outputStream = (__bridge NSOutputStream *)writeStream;
+    inputStream = (__bridge NSInputStream *)readStream;
+    
+    [outputStream setDelegate:self];
+    [inputStream setDelegate:self];
+    
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    [outputStream open];
+    [inputStream open];
+    
+    _connectedLabel.text = @"Connected";
+    [_sendMsgBtn setEnabled:YES];
+    [_connectBtn setEnabled:NO];
+}
+
+- (void)close {
+    NSLog(@"Closing streams.");
+    [inputStream close];
+    [outputStream close];
+    [inputStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [outputStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [inputStream setDelegate:nil];
+    [outputStream setDelegate:nil];
+    inputStream = nil;
+    outputStream = nil;
+    
+    _connectedLabel.text = @"Disconnected";
 }
 
 - (void)didReceiveMemoryWarning {
